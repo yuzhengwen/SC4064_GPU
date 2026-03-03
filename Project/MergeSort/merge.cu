4f31a979a1fdbf37143b30643a41afde3ecfc3e6
Yu Zhengwen <yuzhengwen0217@gmail.com>
2026-03-04 00:06:20 +0800

    added per pass timing

--- Project/MergeSort/merge.cu
+++ Project/MergeSort/merge.cu
@@ -67,17 +67,49 @@ __global__ void gpu_merge_kernel(int *src, int *dst, long long n, long long widt
     while (i < mid)   dst[k++] = src[i++];
     while (j < right) dst[k++] = src[j++];
 }
+
 int *gpu_mergeSort_naive(int *d_data, int *d_temp, int n)
 {
     const int threads = 256;
+    int pass = 1;
+    float total_ms = 0.0f;
+
+    printf("\n  %-6s  %-18s  %-10s  %-10s\n",
+           "Pass", "Width", "Threads", "Time (ms)");
+    printf("  -----------------------------------------------\n");
+
     for (long long width = 1; width < (long long)n; width *= 2) {
         long long num_merges = ((long long)n + 2LL * width - 1) / (2LL * width);
         int blocks = (int)((num_merges + threads - 1) / threads);
+
+        // Create per-pass CUDA events
+        cudaEvent_t ps, pe;
+        CUDA_CHECK(cudaEventCreate(&ps));
+        CUDA_CHECK(cudaEventCreate(&pe));
+
+        CUDA_CHECK(cudaEventRecord(ps));
         gpu_merge_kernel<<<blocks, threads>>>(d_data, d_temp, (long long)n, width);
+        CUDA_CHECK(cudaEventRecord(pe));
+        CUDA_CHECK(cudaEventSynchronize(pe));
         CUDA_CHECK(cudaGetLastError());
-        CUDA_CHECK(cudaDeviceSynchronize());
+
+        float pass_ms = 0.0f;
+        CUDA_CHECK(cudaEventElapsedTime(&pass_ms, ps, pe));
+        total_ms += pass_ms;
+
+        printf("  %-6d  %-18lld  %-10lld  %.3f ms\n",
+               pass, width, num_merges, pass_ms);
+
+        CUDA_CHECK(cudaEventDestroy(ps));
+        CUDA_CHECK(cudaEventDestroy(pe));
+
         int *tmp = d_data; d_data = d_temp; d_temp = tmp;
+        pass++;
     }
+
+    printf("  -----------------------------------------------\n");
+    printf("  Total GPU time: %.3f ms\n\n", total_ms);
+
     return d_data;
 }
 
